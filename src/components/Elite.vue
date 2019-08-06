6084d883a20658b12a3493871faf9d38acb65c17
JH <18468107890@163.com>
2019-08-06 12:53:23 +0800

    上传代码

---
+++ src/components/Elite.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        哈哈哈，和前面类似。
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "elite_content",
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PostList from "../components/PostList"
+import Article from '../components/Article'
+import UserInfo from  '../components/UserInfo'
+import SlideBar from "../components/SlideBar";
+import Elite from '../components/Elite'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            name: 'root',
+            path: '/', // 指定要跳转的路径
+            components: {    // 指定要跳转的组件
+                main: PostList
+            }
+        },
+        {
+            name: 'post_content',
+            path: '/topic/:id&author=:name', // 指定要跳转的路径
+            components: {    // 指定要跳转的组件
+                main: Article,
+                slidebar: SlideBar
+            }
+        },
+        {
+            name: 'user_info',
+            path: '/userinfo/:name', // 指定要跳转的路径
+            components: {    // 指定要跳转的组件
+                main: UserInfo
+            }
+        },
+        {
+            name: 'elite_content',
+            path: '/elite/', // 指定要跳转的路径
+            components: {    // 指定要跳转的组件
+                main: Elite
+            }
+        },
+    ]
+})
